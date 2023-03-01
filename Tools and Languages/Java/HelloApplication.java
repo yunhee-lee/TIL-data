0fad02652c3f9ce682b32529c7e2fb943322dd07
Yunhee Lee <leeyunhee12@gmail.com>
2023-03-01 23:02:24 +0900

    [Upload] The first java program - Hello!

---
+++ Tools and Languages/Java/HelloApplication.java
@@ -0,0 +1,11 @@
+/**
+ * The Hello class implements an application
+ * that simply prints "Hello, Java Application!" to standard output.
+ */
+public class HelloApplication {
+    public static void main(String args[]) {
+        // main method
+        // display the string
+        System.out.println("Hello, Java Application!");
+    }
+}
